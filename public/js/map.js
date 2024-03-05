adce9ae9ccc5f827a4ff2844507b62d34400d9ee
MOHIT <mohitjyouti2003@gmail.com>
2024-03-05 21:02:47 +0530

    add feature

--- public/js/map.js
+++ public/js/map.js
@@ -17,12 +17,12 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/streets-v12",
-    center: [77.2090,28.6139],
+    center: coordinates,
     zoom: 9
 });
 
 const marker = new mapboxgl.Marker({ color: "red" })
-    .setLngLat(coordinates)
+    .setLngLat(listingSchema.geometry.coordinates)
     .setPopup(new mapboxgl.Popup({ offset: 25 })
         .setHTML("<h2>Hello World!</h2>"))
     .addTo(map);
