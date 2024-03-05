6ee260bf0e652277055e3bae6bb0316cadf9fde7
MOHIT <mohitjyouti2003@gmail.com>
2024-03-05 13:08:22 +0530

    add feature

--- public/js/map.js
+++ public/js/map.js
@@ -20,7 +20,7 @@ const map = new mapboxgl.Map({
     center: [77.2090,28.6139],
     zoom: 9
 });
-console.log(coordinates)
+
 const marker = new mapboxgl.Marker({ color: "red" })
     .setLngLat(coordinates)
     .setPopup(new mapboxgl.Popup({ offset: 25 })
